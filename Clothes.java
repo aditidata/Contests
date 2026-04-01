78f7c43be4d2582e57ac7526c1b1ca175ba64de2
aditidata <155631327+aditidata@users.noreply.github.com>
2026-04-01 20:36:03 +0530

    Vacation Clothes Chef is going on a vacation for N N days. Each day, he must wear a fresh set of clothes.
    Whenever a set is worn on day
    d
    d, it is sent for cleaning and becomes available again only on day
    d
    +
    7
    d+7. A set that returns from cleaning on some day may be worn again that same day.
    
    What is the minimum number of sets of clothes Chef must carry so that he always has something to wear for all
    N
    N days?

---
+++ Clothes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Clothes
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		System.out.println(Math.min(N, 7));
+		
+
+	}
+}
